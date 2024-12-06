28dd9b15c37db69b9353225aa8d834b08dfb9779
Adarsh Ranjan <adarsh.ranjan@roro.io>
2024-12-06 09:56:00 +0530

    Initial Commit

---
+++ CryptoMarketTracker/CryptoMarketTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMarketTrackerApp.swift
+//  CryptoMarketTracker
+//
+//  Created by Adarsh Ranjan on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMarketTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
